761f5dbd3941a2b6ebb3fd9617f8462c323f7a2c
Ivan Geier <vte_ivila@uolinc.com>
2022-12-05 13:39:51 -0300

    💄 install and set global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello!</h1>
+    <ThemeProvider theme={defaultTheme} >
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
