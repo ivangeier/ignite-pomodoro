1b38a71e2fe3e0126a8f352367f12731aae7b3c7
Ivan Geier <vte_ivila@uolinc.com>
2022-12-22 13:41:11 -0300

    🔨 Fix minimus time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(1).max(60),
+  minutesAmount: zod.number().min(5).max(60),
 })
 
 type NewCycleFormProps = zod.infer<typeof newCycleFormValidationSchema>
@@ -152,7 +152,7 @@ export function Home() {
             type="number"
             step={5}
             placeholder="00"
-            min={1}
+            min={5}
             max={60}
             disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
